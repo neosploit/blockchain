8cb8415a60a07afe8bfa956c07532b979b39ff9f
drifter1 <37935199+drifter1@users.noreply.github.com>
2020-11-30 15:26:29 +0200

    Update hashing.c

--- src/hashing/hashing.c
+++ src/hashing/hashing.c
@@ -3,52 +3,35 @@
 #include <string.h>
 #include "../../include/hashing.h"
 
-// modulo functions
-int modulo(long int value){
-    int hash;
-
-    hash = value % (1 << (4 * GROUP_LENGTH));
-
-    return hash;
-}
-
-int modulo_add(long int valueX, long int valueY){
-    int hash;
-
-    hash = (valueX + valueY) % (1 << (4 * GROUP_LENGTH));
-
-    return hash;
-}
-
 // conversion functions
-int* hash_to_int_array(const char *hash_string){
-    int *hash;
+unsigned int* hash_to_int_array(const char *hash_string){
+    unsigned int *hash;
     int i, j;
-    char tmp[GROUP_LENGTH + 1];
+    char tmp[HEX_GROUP + 1];
 
-    hash = (int*) calloc((HASH_LENGTH / GROUP_LENGTH), sizeof(int));
+    hash = (unsigned int*) calloc((int)(HEX_LENGTH / HEX_GROUP), sizeof(unsigned int));
 
-    for(i = 0; i < HASH_LENGTH / GROUP_LENGTH; i++){
-        for(j = 0; j < GROUP_LENGTH; j++){
+    for(i = 0; i < (int)(HEX_LENGTH / HEX_GROUP); i++){
+        for(j = 0; j < HEX_GROUP; j++){
             tmp[j] = hash_string[2*i + j];
         }
-
+        
         hash[i] = strtol(tmp, NULL, 16);
     }
 
     return hash;
 }
 
-char* int_array_to_hash(int *hash){
+char* int_array_to_hash(unsigned int *hash){
     char *hash_string;
     int i;
-    char tmp[GROUP_LENGTH + 1];
+    char tmp[HEX_GROUP + 1];
 
-    hash_string = (char*) malloc((HASH_LENGTH + 1) * sizeof(char));
+    hash_string = (char*) malloc((HEX_LENGTH + 1) * sizeof(char));
     
-    sprintf(hash_string, "%0*x", GROUP_LENGTH, hash[0]);
-    for(i = 1; i < HASH_LENGTH/GROUP_LENGTH; i++){
-        sprintf(tmp, "%0*x", GROUP_LENGTH, hash[i]);
+    sprintf(hash_string, "%0*x", HEX_GROUP, hash[0]);
+    for(i = 1; i < HEX_LENGTH/HEX_GROUP; i++){
+        sprintf(tmp, "%0*x", HEX_GROUP, hash[i]);
         strcat(hash_string, tmp);
     }
 
